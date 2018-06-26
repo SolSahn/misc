2283b437e23a6c9c3fae4eea990877e217f352db
SolSahn <solsahn@gmail.com>
2018-06-26 05:06:02 -0400

    Update archscript.sh

--- archscript.sh
+++ archscript.sh
@@ -11,6 +11,7 @@ mount /dev/sda3 /mnt/home
 clear
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
 echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+locale-gen
 clear
 pacstrap /mnt base base-devel
 genfstab /mnt>> /mnt/etc/fstab
